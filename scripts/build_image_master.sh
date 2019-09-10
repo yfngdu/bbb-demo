e8eea807479e2094134f75be8082394d1d29df06
yfngdu <amy.du@blindsidenetworks.com>
2019-09-10 14:54:15 -0400

    download greenlight in docker

--- scripts/build_image_master.sh
+++ scripts/build_image_master.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker build -t bigbluebutton/greenlight:master .
+docker build -t bigbluebutton/greenlight:master https://github.com/bigbluebutton/greenlight.git
 
-docker login -e zachary.chai@blindsidenetworks.com -u $DOCKER_USER -p $DOCKER_PASS
+docker login -e amy.du@blindsidenetworks.com -u $DOCKER_USER -p $DOCKER_PASS
 docker push bigbluebutton/greenlight:master
 
 docker logout
