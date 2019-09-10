e4e03017904dd4ff12cd9cdf5ae4834840b8ac30
yfngdu <amy.du@blindsidenetworks.com>
2019-09-10 15:37:43 -0400

    comment out docker push

--- scripts/build_image_master.sh
+++ scripts/build_image_master.sh
@@ -2,7 +2,7 @@
 
 docker build -t bigbluebutton/greenlight:master https://github.com/bigbluebutton/greenlight.git
 
-docker login -e amy.du@blindsidenetworks.com -u $DOCKER_USER -p $DOCKER_PASS
-docker push bigbluebutton/greenlight:master
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+# docker push bigbluebutton/greenlight:master
 
 docker logout
