c8211d217216dfb92fa00989e09d86fb85473c66
yfngdu <amy.du@blindsidenetworks.com>
2019-09-10 16:41:58 -0400

    remove deploy image

--- scripts/build_image_master.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker build -t amyblindside1/greenlight:master https://github.com/bigbluebutton/greenlight.git
-
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-docker push amyblindside1/greenlight:master
-
-docker logout
--- scripts/default_start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rake db:migrate
-
-exec bundle exec puma -C config/puma.rb
